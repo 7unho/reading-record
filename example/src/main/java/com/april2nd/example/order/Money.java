8cec069c2a8c010d5b413548926ff07261826f58
7unho <rlawnsgh8395@naver.com>
2025-04-02 13:19:22 +0900

    :sparkles: Feat: Domain 생성

--- example/src/main/java/com/april2nd/example/order/Money.java
+++ example/src/main/java/com/april2nd/example/order/Money.java
@@ -1,2 +1,9 @@
-package com.april2nd.example.order;public enum Money {
+package com.april2nd.example.order;
+
+public class Money {
+    int price;
+
+    public Money(int price) {
+        this.price = price;
+    }
 }
--- example/src/main/java/com/april2nd/example/order/OrderLine.java
+++ example/src/main/java/com/april2nd/example/order/OrderLine.java
@@ -1,2 +1,26 @@
-package com.april2nd.example.order;public class OrderLine {
+package com.april2nd.example.order;
+
+import com.april2nd.example.product.Product;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class OrderLine {
+    private Product product;
+    private int price;
+    private int quantity;
+    private int amounts;
+
+    @Builder
+    public OrderLine(Product product, int price, int quantity) {
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+        this.amounts = calculateAmounts();
+    }
+
+    private int calculateAmounts() {
+        return price * quantity;
+    }
+
 }
