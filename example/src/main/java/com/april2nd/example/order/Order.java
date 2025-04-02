5cd7b0ef9c05df26188aeae07d977e406657c3dd
7unho <rlawnsgh8395@naver.com>
2025-04-02 13:22:07 +0900

    :sparkles: Feat: Order 도메인 로직 고도화
    요구사항에 맞춘 제약 추가

--- example/src/main/java/com/april2nd/example/order/Order.java
+++ example/src/main/java/com/april2nd/example/order/Order.java
@@ -3,23 +3,73 @@ package com.april2nd.example.order;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class Order {
     private OrderStatus state;
     private ShippingInfo shippingInfo;
-
+    private List<OrderLine> orderLines;
+    private Money totalAmounts;
     @Builder
-    public Order(OrderStatus state, ShippingInfo shippingInfo) {
+    public Order(OrderStatus state, List<OrderLine> orderLines, ShippingInfo shippingInfo) {
         this.state = state;
+        setOrderLines(orderLines);
+        setShippingInfo(shippingInfo);
+    }
+
+    private void setShippingInfo(ShippingInfo shippingInfo) {
+        if (shippingInfo == null) {
+            throw new IllegalArgumentException("no shipping info");
+        }
         this.shippingInfo = shippingInfo;
     }
 
-    public void changeShippingInfo(ShippingInfo newShippingInfo) {
-        //
-        if (!state.isShippingChangeable()) {
-            throw new IllegalStateException("can't change shipping in " + state);
-        }
+    private void setOrderLines(List<OrderLine> orderLines) {
+        verifyAtLeastOneOrMoreOrderLines(orderLines);
+        this.orderLines = orderLines;
+        calculateTotalAmounts();
+    }
 
-        this.shippingInfo = newShippingInfo;
+    /*
+        총 주문 금액 계산
+     */
+    private void calculateTotalAmounts() {
+        int sum = orderLines.stream()
+                .mapToInt(x -> x.getAmounts())
+                .sum();
+        this.totalAmounts = new Money(sum);
+    }
+
+    /*
+        요구사항. 최소 한 종류 이상의 상품을 주문해야 한다.
+     */
+    private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
+        if (orderLines == null || orderLines.isEmpty()) {
+            throw new IllegalArgumentException("no OrderLine");
+        }
+    }
+
+    public void changeShippingInfo(ShippingInfo newShippingInfo) {
+        verifyNotYetShipped();
+        setShippingInfo(newShippingInfo);
+    }
+
+    private void verifyNotYetShipped() {
+        if (state.isShippingChangeable()) {
+            throw new IllegalStateException("already shipped");
+        }
+    }
+
+    public void changeShipped() {
+
+    }
+
+    public void cancel() {
+
+    }
+
+    public void completePayment() {
+
     }
 }
